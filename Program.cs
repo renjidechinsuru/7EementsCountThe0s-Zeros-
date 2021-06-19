5fc8556efb07f82007e0ff1ae962fd8af25946f7
Ai Uragaki <ai.uragaki.nz@gmail.com>
2021-06-19 23:52:35 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _7EementsCountThe0s_Zeros_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //Declare an array of 7 elements. Take input from user to initialize the array and count the 0s(Zeros) in the array.
+        {
+            int count = 0;
+                int numbers = 7;
+
+            for (int i = 0; i < numbers; i++)
+            {
+
+                Console.WriteLine("Please Enter a number");
+                string input = Console.ReadLine();
+                if (Convert.ToInt32(input) == 0)
+                
+
+                
+            
+                count = count + 1;
+            }
+            {
+                Console.WriteLine("there is " + count + " of 0s");
+            }
+
+
+        }
+    }
+}
+            
